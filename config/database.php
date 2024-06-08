8b0362ce4375acd56e3d589838e87e26efa769c7
Chaima BEKHAOUDA <137110936+chaima-bekhaouda@users.noreply.github.com>
2024-06-08 17:51:47 +0200

    refactor(Database): refactor PDO connection script into Database class

--- config/database.php
+++ config/database.php
@@ -1,16 +1,34 @@
 <?php
-// Connexion à la base de données
-$host = 'localhost';
-$dbname = 'donorcheck';
-$username = 'root';
-$password = '';
 
-// Création de la connexion
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+class Database
+{
+    private PDO $pdo;
 
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
+    /**
+     * Database constructor.
+     *
+     * @param string $host
+     * @param string $dbname
+     * @param string $username
+     * @param string $password
+     */
+    public function __construct(string $host, string $dbname, string $username, string $password)
+    {
+        try {
+            $this->pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Failed to connect to the database: " . $e->getMessage());
+        }
+    }
+
+    /**
+     * Get the PDO instance.
+     *
+     * @return PDO
+     */
+    public function getPdo(): PDO
+    {
+        return $this->pdo;
+    }
 }
-?>
